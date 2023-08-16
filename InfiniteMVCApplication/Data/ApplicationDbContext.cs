881e498f961c666c4d44ffb2d2ffbe9e48a2c888
Chandrashekar Jogiyappa <chandrashekarj@infinite.com>
2023-08-16 23:09:57 +0530

    Add project files.

---
+++ InfiniteMVCApplication/Data/ApplicationDbContext.cs
@@ -0,0 +1,46 @@
+﻿using InfiniteMVCApplication.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InfiniteMVCApplication.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Category { get; set; }
+        public DbSet<CategoryItem> CategoryItem { get; set; }
+        public DbSet<MediaType> MediaType { get; set; }
+        public DbSet<UserCategory> UserCategory { get; set; }
+        public DbSet<Content> Contents { get; set; }
+
+
+
+
+
+    }
+
+    public class ApplicationUser : IdentityUser
+    {
+        [StringLength(250)]
+        public string FirstName { get; set; }
+        [StringLength(250)]
+        public string LastName { get; set; }
+        [StringLength(500)]
+        public string Address1 { get; set; }
+        [StringLength(500)]
+        public string Address2 { get; set; }
+        [StringLength(6)]
+        public string PostCode { get; set; }
+        [ForeignKey("UserId")]
+        public virtual ICollection<UserCategory> UserCategory{ get; set; }
+
+    }
+}
---
+++ InfiniteMVCApplication/Data/Migrations/20230816173045_addCustomTables.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InfiniteMVCApplication.Data.Migrations
+{
+    public partial class addCustomTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Category",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ThumbnailImagePath = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Category", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MediaType",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    ThumbnailImagePath = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MediaType", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserCategory",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    CategoryId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserCategory", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserCategory_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UserCategory_Category_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Category",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CategoryItem",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    CategoryId = table.Column<int>(type: "int", nullable: false),
+                    MediaTypeId = table.Column<int>(type: "int", nullable: false),
+                    DateTimeItemReleased = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    MediTypeId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CategoryItem", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_CategoryItem_Category_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Category",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CategoryItem_MediaType_MediTypeId",
+                        column: x => x.MediTypeId,
+                        principalTable: "MediaType",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Contents",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    HTMLContent = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    VideoLink = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CategoryItemId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contents", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Contents_CategoryItem_CategoryItemId",
+                        column: x => x.CategoryItemId,
+                        principalTable: "CategoryItem",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CategoryItem_CategoryId",
+                table: "CategoryItem",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CategoryItem_MediTypeId",
+                table: "CategoryItem",
+                column: "MediTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contents_CategoryItemId",
+                table: "Contents",
+                column: "CategoryItemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserCategory_CategoryId",
+                table: "UserCategory",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserCategory_UserId",
+                table: "UserCategory",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contents");
+
+            migrationBuilder.DropTable(
+                name: "UserCategory");
+
+            migrationBuilder.DropTable(
+                name: "CategoryItem");
+
+            migrationBuilder.DropTable(
+                name: "Category");
+
+            migrationBuilder.DropTable(
+                name: "MediaType");
+        }
+    }
+}
